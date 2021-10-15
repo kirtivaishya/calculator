4ae3366acd9d986ab8c359146f0623205a0ede8e
kirti vaishya <kirti.vaishya@db.com>
2021-10-15 11:16:28 +0530

    layout for calculator

---
+++ js/dist/function.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// **********Add the value to input ***********
+button.addEventListener('click', function (event) {
+  event.preventDefault();
+  var buttonList = document.querySelectorAll("button");
+});
