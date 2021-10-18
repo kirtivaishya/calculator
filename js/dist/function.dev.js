7abbadf8537fc393c71310a488cf638ceb75f4ca
kirti vaishya <kirti.vaishya@db.com>
2021-10-18 17:05:09 +0530

    updated calculator function and logic

--- js/dist/function.dev.js
+++ js/dist/function.dev.js
@@ -1,7 +1,77 @@
 "use strict";
 
 // **********Add the value to input ***********
-button.addEventListener('click', function (event) {
-  event.preventDefault();
-  var buttonList = document.querySelectorAll("button");
+var inputtext = document.getElementsByTagName("input")[0]; //console.log(inputtext);
+
+var buttons = document.querySelectorAll("button"); //console.log(buttons);
+
+var outputValue = inputtext.value;
+var currentOperator = "";
+var previousOperator = "";
+buttons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    event.preventDefault(); //console.log(button.value); 
+
+    var buttonContent = button.innerText;
+
+    if (button.classList.contains("btn-el")) {
+      reset();
+    }
+
+    if (button.classList.contains("btn-operation")) {
+      if (previousOperator != "") {
+        var outputValues = outputValue.split(previousOperator);
+        currentOperator = buttonContent;
+        outputValue = calculate(outputValues[0], outputValues[1], previousOperator);
+        inputtext.value = outputValue;
+        outputValue = outputValue + currentOperator;
+        previousOperator = currentOperator;
+      } else {
+        previousOperator = buttonContent;
+        outputValue = outputValue + buttonContent;
+        inputtext.value = outputValue;
+      } // submit(operator);
+
+    }
+
+    if (button.name == "number") {
+      if (outputValue != "") {
+        outputValue = outputValue + buttonContent;
+        inputtext.value = outputValue;
+      } else {
+        inputtext.value = button.innerText;
+        outputValue = inputtext.value;
+      }
+    }
+  });
 });
+
+var reset = function reset() {
+  inputtext.value = "";
+  outputValue = "";
+  previousOperator = "";
+  return;
+}; //  const submit=(operator)=>{
+//         console.log("on sumbit");
+//         return;
+//  }
+
+
+function calculate(firstOperand, secondOperand, operator) {
+  firstOperand = parseFloat(firstOperand);
+  secondOperand = parseFloat(secondOperand);
+
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } else if (operator === '-') {
+    return firstOperand - secondOperand;
+  } else if (operator === '*') {
+    return firstOperand * secondOperand;
+  } else if (operator === '/') {
+    return firstOperand / secondOperand;
+  } else if (operator === '%') {
+    return secondOperand * 0.01;
+  }
+
+  return secondOperand;
+}
--- js/function.js
+++ js/function.js
@@ -1,24 +1,78 @@
 // **********Add the value to input ***********
 const inputtext = document.getElementsByTagName("input")[0];
-console.log(inputtext);
+//console.log(inputtext);
 const buttons= document.querySelectorAll("button");
-console.log(buttons);
+//console.log(buttons);
+let outputValue=inputtext.value;
+let currentOperator="";
+let previousOperator="";
 buttons.forEach(button => {
 
        button.addEventListener('click',(event)=>{
             event.preventDefault();          
-            console.log(button.value); 
-            const outputValue=0;
-            const buttonContent= button.innerText
-            if (typeof(buttonContent)=="number") {
-                outputValue=inputtext.value+""+outputValue;
-            } else {
-                outputValue=inputtext.value+""+outputValue;
+            //console.log(button.value); 
+            
+            const buttonContent= button.innerText;
+            if(button.classList.contains("btn-el")){
+                reset();
             }
-            inputtext.value = button.innerText;
+            if(button.classList.contains("btn-operation")){
+                if(previousOperator!=""){
+                   const outputValues= outputValue.split(previousOperator);
+                   currentOperator=buttonContent;
+                   outputValue=calculate(outputValues[0],outputValues[1],previousOperator);
+                   inputtext.value=outputValue;
+                   outputValue=outputValue+currentOperator;
+                   previousOperator=currentOperator;
+                }else{
+                    previousOperator=buttonContent;
+                    outputValue=outputValue+buttonContent;
+                    inputtext.value=outputValue;
+
+                }
+               // submit(operator);
+            }
+            if(button.name=="number"){
+                if (outputValue!="") {
+                    outputValue=outputValue+buttonContent;
+                    inputtext.value=outputValue;
+                } else {
+                    inputtext.value = button.innerText;
+                    outputValue=inputtext.value;
+                } 
+
+            }
+           
         });
 });
 
- 
+ const reset=()=>{
+    inputtext.value="";
+    outputValue="";
+    previousOperator="";
+    return;
+ }
 
+//  const submit=(operator)=>{
 
+//         console.log("on sumbit");
+//         return;
+//  }
+
+ function calculate(firstOperand, secondOperand, operator) {
+     firstOperand=parseFloat(firstOperand);
+     secondOperand=parseFloat(secondOperand);
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }else if (operator === '%') {
+        return secondOperand*0.01;
+      }
+  
+    return secondOperand;
+  }
