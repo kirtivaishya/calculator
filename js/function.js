d116ed56df54073e34c85cf13260ba4280351335
kirti vaishya <kirti.vaishya@db.com>
2021-10-18 09:17:04 +0530

    function added to add value on output screen

--- js/function.js
+++ js/function.js
@@ -1,5 +1,24 @@
 // **********Add the value to input ***********
-button.addEventListener('click',(event)=>{
-    event.preventDefault();
-   const buttonList= document.querySelectorAll("button");
+const inputtext = document.getElementsByTagName("input")[0];
+console.log(inputtext);
+const buttons= document.querySelectorAll("button");
+console.log(buttons);
+buttons.forEach(button => {
+
+       button.addEventListener('click',(event)=>{
+            event.preventDefault();          
+            console.log(button.value); 
+            const outputValue=0;
+            const buttonContent= button.innerText
+            if (typeof(buttonContent)=="number") {
+                outputValue=inputtext.value+""+outputValue;
+            } else {
+                outputValue=inputtext.value+""+outputValue;
+            }
+            inputtext.value = button.innerText;
+        });
 });
+
+ 
+
+
